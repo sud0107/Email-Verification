6203d80f6785754a76a8285990ea233ac9e780d7
sudhanshu <jhasudhanshu08@gmail.com>
2022-04-08 16:36:26 +0530

    little change

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,32 @@
 const User = require("../models/user");
+const mailgun = require('mailgun-js')
 
-co
+
+var api_key = 'XXXXXXXXXXXXXXXXXXXXXXX';
+var domain = 'www.mydomain.com';
+const mg = mailgun({apiKey: api_key, domain: domain});
+
+// exports.signup = (req, res) => {
+//     console.log(req.body);
+//     const {name, email, password} = req.body;
+//     User.findOne({email}).exec((err, user) => {
+//         if(user) {
+//             return res.status(404).json({error: "User with this email is already exist"});
+//         }
+//         let newUser = new User({name, email, password});
+//         newUser.save((err, success) => {
+//             if(err) {
+//                 console.log("Error in signup: ", err)
+//                 return res.status(404).json({error: err})
+//             }
+//             res.json({
+//                 message: "Sgnup Successful"
+//             })
+//         })
+//     });
+    
+// }
+
 
 exports.signup = (req, res) => {
     console.log(req.body);
